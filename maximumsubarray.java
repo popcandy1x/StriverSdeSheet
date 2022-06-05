3fdca06904c7724c578f328749c4ac42fc9ebdae
Venkatesh Jaiswal <popcandy1x@gmail.com>
2022-06-05 11:48:40 +0530

    Added code for the problem maximum subarray sum

--- maximumsubarray.java
+++ maximumsubarray.java
@@ -4,17 +4,17 @@ import java.io.*;
 public class maximumsubarray{
 	
 	public static long maxSubarraySum(int[] arr, int n) {
-		long[] b=new long[arr.length];
-        b[0]=arr[0];
-        long maxSubAraySum=b[0];
-        for(int i=1;i<arr.length;i++){
-            b[i]=arr[i]+b[i-1];
-            if(b[i]<arr[i]){
-                b[i]=arr[i];
+    		long[] b=new long[arr.length];
+            b[0]=arr[0];
+            long maxSubAraySum=b[0];
+            for(int i=1;i<arr.length;i++){
+                b[i]=arr[i]+b[i-1];
+                if(b[i]<arr[i]){
+                    b[i]=arr[i];
+                }
+                maxSubAraySum=Math.max(b[i],maxSubAraySum);
             }
-            maxSubAraySum=Math.max(b[i],maxSubAraySum);
-        }
-        return maxSubAraySum<0?0:maxSubAraySum;
+            return maxSubAraySum<0?0:maxSubAraySum;
 	}
 
 }
