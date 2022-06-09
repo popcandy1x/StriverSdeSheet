b80504e7169d82c6dde23590926ab8de01ea3c66
Venkatesh Jaiswal <popcandy1x@gmail.com>
2022-06-09 17:02:45 +0530

    Added code for the problem longest consecutive sequence

---
+++ longestconsecutivesequence.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int lengthOfLongestConsecutiveSequence(int[] arr, int N) {
+        if(arr.length==0){
+            return 0;
+        }
+        Set<Integer> s=new HashSet<>();
+        int count=0;
+        int maxConsecutiveLength=0;
+        for(int i=0;i<arr.length;i++){
+           s.add(arr[i]);
+        }
+        for(int i:s){
+            if(!s.contains(i-1)){
+                int j=i+1;
+                while(s.contains(j)){
+                    count++;
+                    i++;;
+                    j=i+1;
+                }             
+                maxConsecutiveLength=Math.max(count,maxConsecutiveLength);
+                count=0;
+            }
+        }
+        return maxConsecutiveLength+1;
+    }
+}
