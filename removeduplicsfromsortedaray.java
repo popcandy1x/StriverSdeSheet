8853a9bddb2bfdfbd96dabaa095d0a9d0e10ab1f
Venkatesh Jaiswal <popcandy1x@gmail.com>
2022-06-13 00:26:21 +0530

    Added code for the problem remove duplicates from sorted array

---
+++ removeduplicsfromsortedaray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class removesduplicsfromsortedaray{
+	 public static int removeDuplicates(ArrayList<Integer> arr,int n) {
+        for(int i=arr.size()-1;i>0;i--){
+            if(arr.get(i)==arr.get(i-1)){
+                arr.remove(i);
+            }
+        }
+        return arr.size();
+    }
+}
